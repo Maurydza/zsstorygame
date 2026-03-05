61165baf40f98068b50143469a1446a8f5bcb809
Kajtop <kajetan.piwowarczyk.priv@gmail.com>
2026-03-05 20:13:20 +0100

    Eventually i managed to do the first dialogue so next will be much easier. The dialogue is clear and working perfectly.

--- Assets/Scripts/NpcNeutral.cs
+++ Assets/Scripts/NpcNeutral.cs
@@ -12,6 +12,8 @@ public class NpcNeutral : MonoBehaviour
     private int index;
     private InputAction interraction;
 
+    private Coroutine typingCoroutine;
+
     public GameObject continueButton;
     public float wordSpeed;
     public  bool playerIsClose;
@@ -20,32 +22,43 @@ public class NpcNeutral : MonoBehaviour
 
     void Start()
     {
-
+        // for pressing E to interact with the npc
         interraction = InputSystem.actions.FindAction("Interact");
-        interraction.Enable();
     }
     void Update()
     {
-        if(interraction.IsPressed() && playerIsClose)
+        // if the player is close and presses the interact button, the dialogue panel will appear and the dialogue will start typing
+        if (interraction.WasPressedThisFrame() && playerIsClose)
         {
+            
             if (dialoguePanel.activeInHierarchy)
             {
                 zeroExit();
+
             }
             else
             {
                 dialoguePanel.SetActive(true);
-                StartCoroutine(Typing());
+
+                if (typingCoroutine != null)
+                {
+                    StopCoroutine(typingCoroutine); // Stop the previous typing coroutine if it's still running
+                }
+
+
+                typingCoroutine = StartCoroutine(Typing()); // Start the typing coroutine
             }
         }
 
+        // if the dialogue is fully typed, the continue button will appear
+
         if (dialogueText.text == dialogueLines[index])
         {
-            continueButton.SetActive(true);
+            continueButton.SetActive(true); 
         }
     }
 
-
+    // this function will reset the dialogue and hide the dialogue panel
     public void zeroExit()
     {
         dialogueText.text = "";
@@ -53,8 +66,12 @@ public class NpcNeutral : MonoBehaviour
         dialoguePanel.SetActive(false);
     }
 
+    
+    // this function will type the dialogue letter by letter with a delay between each letter
     IEnumerator Typing()
     {
+        dialogueText.text = "";
+
         foreach (char letter in dialogueLines[index].ToCharArray()) 
         {
             dialogueText.text += letter;
@@ -63,6 +80,7 @@ public class NpcNeutral : MonoBehaviour
         }
     }
 
+    // this function will be called when the continue button is pressed, it will move to the next line of dialogue or exit if it's the last line
     public void NextLine()
     {
 
@@ -72,6 +90,11 @@ public class NpcNeutral : MonoBehaviour
         {
             index++;
             dialogueText.text = "";
+            if (typingCoroutine != null)
+            {
+                StopCoroutine(typingCoroutine);
+            }
+
             StartCoroutine(Typing());
         }
         else
@@ -80,22 +103,26 @@ public class NpcNeutral : MonoBehaviour
         }
     }
 
+    // this function will be called when the player enters the trigger area of the npc, it will set the playerIsClose variable to true
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+
             playerIsClose = true;
-            dialoguePanel.SetActive(true);
-            StartCoroutine(Typing());
+
+            
         }
     }
 
+    // this function will be called when the player exits the trigger area of the npc, it will set the playerIsClose variable to false and reset the dialogue
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             playerIsClose = false;
             zeroExit();
+            
         }
     }
 }
